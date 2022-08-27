fb9e8d44aef33d1936aba2853a454b0af622143b
Administrator <julien.garderon@gmail.com>
2022-08-27 18:23:42 +0200

    API modulaire 'configuration' opérationnelle

---
+++ src/api/configuration/configuration.go
@@ -0,0 +1,105 @@
+package configuration
+
+import (
+  "net/http"
+  "io/ioutil"
+  "encoding/json"
+  "sync"
+  // "fmt"
+  // -----------
+  "api"
+  "configuration"
+  "httpresponse"
+  "logger"
+)
+
+type HandlerApi struct {
+  Logger *logger.Logger
+  ConfMutext *sync.RWMutex
+  Conf *configuration.Conf
+}
+
+func ( handlerApi HandlerApi ) ServeHTTP ( w http.ResponseWriter, r *http.Request ) {
+  httpResponse := httpresponse.Response {
+    Code: http.StatusInternalServerError,
+    MessageError: "an unexpected error has occurred",
+  }
+  defer httpResponse.Respond( handlerApi.Logger, w )
+  handlerApi.ConfMutext.RLock()
+  auth := api.VerifyAuthorization( handlerApi.Conf, r )
+  handlerApi.ConfMutext.RUnlock()
+  if auth {
+    httpResponse.Code = http.StatusUnauthorized
+    httpResponse.MessageError = "you must be authentified"
+    return
+  }
+  switch r.Method  {
+    case http.MethodGet:
+      handlerApi.Get( &httpResponse, r )
+    case http.MethodPatch:
+      handlerApi.Patch( &httpResponse, r )
+   }
+}
+
+func ( handlerApi *HandlerApi ) Patch( httpResponse *httpresponse.Response, r *http.Request ) {
+  handlerApi.ConfMutext.Lock()
+  defer handlerApi.ConfMutext.Unlock()
+  defer handlerApi.Logger.Infof( "Patch conf asked" )
+  if contentType := r.Header.Get("Content-type"); contentType != "application/json" {
+    httpResponse.Code = 400
+    httpResponse.MessageError = "you must have 'application/json' content-type header"
+    return
+  }
+  body, err := ioutil.ReadAll( r.Body )
+  if err != nil {
+    defer handlerApi.Logger.Warningf( "Patch conf asked ; can't read body : %v", err )
+    httpResponse.Code = 500
+    httpResponse.MessageError = "the request's body is an invalid"
+    return
+  }
+  var f interface{}
+  err = json.Unmarshal( body, &f )
+  if err != nil {
+    defer handlerApi.Logger.Warningf( "Patch conf asked ; can't parse body : %v", err )
+    httpResponse.Code = 400
+    httpResponse.MessageError = "the request's body is an invalid"
+    return
+  }
+  o := f.( map[string]interface{} )
+  for key, value := range o {
+    switch key {
+    case "delay":
+      switch value.(type) {
+      case float64:
+        delay := int(value.(float64))
+        if delay >= 5 && delay <= 60 {
+          defer handlerApi.Logger.Warningf( "Patch conf executed, delay changed ; new value : %v", delay )
+          handlerApi.Conf.DelayCleaningContainers = delay
+          continue
+        } else {
+          httpResponse.Code = http.StatusBadRequest
+          httpResponse.MessageError = "value of delay invalid : int between 5 and 60 (seconds)"
+          return
+        }
+      default:
+        httpResponse.Code = http.StatusBadRequest
+        httpResponse.MessageError = "type's value of delay invalid"
+        return
+      }
+    default:
+      httpResponse.Code = 501
+      httpResponse.MessageError = "at least one key is invalid"
+      return
+    }
+  }
+  httpResponse.Code = 202
+}
+
+func ( handlerApi *HandlerApi ) Get( httpResponse *httpresponse.Response, r *http.Request ) {
+  handlerApi.ConfMutext.RLock()
+  defer handlerApi.ConfMutext.RUnlock()
+  defer handlerApi.Logger.Infof( "Conf asked" )
+  httpResponse.Code = http.StatusOK
+  confToJson := *handlerApi.Conf
+  httpResponse.Payload = confToJson
+}
