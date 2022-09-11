464778e1923729aa3815cb225c837100bd0524f4
Administrator <julien.garderon@gmail.com>
2022-09-11 19:31:00 +0200

    Construction architecture WebComponents

---
+++ content/main.js
@@ -0,0 +1,172 @@
+"use strict";
+
+// -----------------------------------------------
+
+class FaassConfiguration {
+  static apiInternalPath = '/api/configuration'; 
+  headers = new Headers();
+  distantConf = null; 
+  inProgress = false; 
+  constructor( password ) {}
+  refresh() { 
+    this.inProgress = true; 
+    return this.#getWebRequest()
+      .then( 
+        c => {
+          this.distantConf = c; 
+          this.inProgress = false; 
+          return Promise.resolve(); 
+        }
+      )
+      .catch( 
+        e => {
+          this.inProgress = false; 
+          if ( typeof e == "number" ) {
+            console.error( `err refresh, status code : ${e}` ); 
+            return Promise.reject( `invalid status code (${e})` ); 
+          } else { 
+            console.error( `err refresh, internal error : ${e}` ); 
+            return Promise.reject( `invalid response body` ); 
+          }
+        }
+      )
+    ;
+  }
+  #getWebRequest() {
+    this.headers.set( 
+      'Authorization', 
+      document
+        .getElementById( 'auth_form_conf' )
+        .value 
+    ); 
+    const request = new Request( 
+    window.location.origin+FaassConfiguration.apiInternalPath, 
+      {
+        method: 'GET', 
+        headers: this.headers 
+      }
+    );
+    return fetch( request )
+      .then( 
+        r => {
+          if (r.status === 200) {
+            return r.json(); 
+          } else {
+            return Promise.reject( r.status );
+          }
+        }
+      )
+  }
+}
+
+// -----------------------------------------------
+
+class TemplateGeneric extends HTMLElement {
+  constructor() {
+    super();
+    this.idTemplate = Object.getPrototypeOf( this )
+      .constructor
+      .idTemplate; 
+    const template = this.deal( 
+      document.getElementById( this.idTemplate )
+        .content
+        .cloneNode(true)
+    );  
+    const shadowRoot = this.attachShadow( {mode: 'open'} )
+      .appendChild( template );
+  } 
+  deal( template ) {
+    return template; 
+  }
+}
+
+// -----------------------------------------------
+
+class TemplateConfGet extends TemplateGeneric { 
+  static idTemplate = 't_conf_get'; 
+  deal( template ) {
+    if ( this.attributes.getNamedItem( 'last' ) != null ) {
+      var a = this.attributes.removeNamedItem( 'last' ); 
+      template.querySelector('input').attributes.setNamedItem( a ); 
+    } 
+    template.querySelector('input').addEventListener(
+      "click", 
+      evt => goTo( evt.target.getAttribute( 'last' ) )
+    )
+    return template;
+  }
+}
+customElements.define( 'conf-get', TemplateConfGet );
+
+// -----------------------------------------------
+
+class TemplateConfEdit extends TemplateGeneric { 
+  static idTemplate = 't_conf_edit';
+}
+customElements.define( 'conf-edit', TemplateConfEdit );
+
+// -----------------------------------------------
+
+class TemplateError extends TemplateGeneric { 
+  static idTemplate = 't_error'; 
+  deal( template ) {
+    var m = 'an unexpected error was found';
+    if ( this.attributes.getNamedItem( 'detail' ) != null ) {
+      m = this.attributes.getNamedItem( 'detail' ).value; 
+    } 
+    template.querySelector('.detail').innerHTML = m; 
+    return template;
+  }
+}
+customElements.define( 'error-detail', TemplateError );
+
+// -----------------------------------------------
+
+function goTo( part, ...rest ) {
+  switch ( part ) {
+    case 'error': 
+      document.getElementById('content').innerHTML = `<error-detail detail="${rest[0]}"></error-detail>`;
+      break 
+    case 'before-refresh':
+      document.getElementById('content').innerHTML = '<conf-get last="refresh"></conf-get>';
+      break
+    case 'refresh': 
+      window.FaassConfiguration.refresh()
+        .then( 
+          _ => document.getElementById('content').innerHTML = `<conf-edit></conf-edit>` 
+        )
+        .catch( 
+          e => document.getElementById('content').innerHTML = `
+            <error-detail detail="${e}"></error-detail>
+            <conf-get last="refresh"></conf-get>
+          `
+        );
+      break
+  }
+}
+
+// -----------------------------------------------
+
+window.addEventListener( 
+  "load", 
+  () => {
+    window.FaassConfiguration = new FaassConfiguration();
+    document.getElementById( 'link_to_edit_conf' ).addEventListener( 
+      'click',
+      evt => goTo( 'refresh' )
+    ); 
+    document.getElementById( 'link_to_edit_routes' ).addEventListener( 
+      'click',
+      evt => goTo( 'error', 'not implemented' )
+    ); 
+    document.getElementById( 'link_to_request' ).addEventListener( 
+      'click',
+      evt => goTo( 'error', 'not implemented' )
+    ); 
+    goTo( 'before-refresh' ); 
+  }
+);
+
+
+
+
